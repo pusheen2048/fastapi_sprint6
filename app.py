3cb2ecb750974e6fd475d478bab0063a4ac8bb18
pusheen1024 <rita26-10@mail.ru>
2026-02-26 22:11:04 +0400

    feat: create app

--- app.py
+++ app.py
@@ -1,25 +1,23 @@
-import asyncio
-import uvicorn
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
-app = FastAPI(root_path="/api/v1")
-app.add_middleware(
-    CORSMiddleware, # type: ignore
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+'''from api.base import router as base_router
+from api.categories import categories_router
+from api.auth import users_router
+from api.locations import locations_router'''
 
 
-async def run() -> None:
-    config = uvicorn.Config("main:app", host="0.0.0.0", port=8000, reload=False)
-    server = uvicorn.Server(config=config)
-    tasks = (asyncio.create_task(server.serve()), )
-    await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
-
-
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(run())
+def create_app():
+    app = FastAPI(root_path="/api/v1")
+    app.add_middleware(
+        CORSMiddleware, 
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    '''app.include_router(base_router, prefix="/base")
+    app.include_router(categories_router, prefix="/categories")
+    app.include_router(users_router, prefix="/users")
+    app.include_router(locations_router, prefix="/locations")'''
+    return app
